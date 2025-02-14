8dac8dc1b0840a1885ab12ddd4d74031ef9ef49d
Jacob Thornton <31422741+jactho2025@users.noreply.github.com>
2025-02-15 01:16:27 +1300

    Learnt scope and reorganised ChallengeProject.

---
+++ TestProject6/Program.cs
@@ -0,0 +1,24 @@
+﻿bool flag = true;
+int value = 0;
+if (flag) {
+    Console.WriteLine($"Inside the code block: {value}");
+}
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+Console.WriteLine("");
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+bool found = false;
+int total = 0;
+foreach (int number in numbers) {
+    total += number;
+    if (number == 42) {
+        found = true;
+    }
+}
+
+if (found) {
+    Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
